7d83c7ed76e71fce9d546fc084d131519e3f708b
jh <jasperhawks@gmail.com>
2021-06-02 15:59:12 -0400

    Fixed finding neighbouring cells and beginning work on change one cell to another.

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ gridArr = [list(range(10)) for y in range (10)] # Instantiate a 2D array
 for rows in range(len(gridArr)):
     for cols in range(len(gridArr)):
 
-        gridArr[rows][cols] = "x" # 
+        gridArr[rows][cols] = "x" 
 
 def drawGrid(modX, modY):
 
-    if modX == -1 and modY == -1: #This IF statment isn't working maybe its because python is insane and doesn't like () or I crapped up somewhere
+    if modX == -1 and modY == -1: 
         y = 0
         while y != 10:
             for x in range(len(gridArr)):
@@ -71,85 +71,89 @@ def logic():
     
     neighbours = []
 
-    # These for loops add the coordinates of the os
+    # These for loops iterate through the entire array and find the neighbours of 
+    # the current cells
     for x in range(len(gridArr)):
             for y in range(len(gridArr)):
-                #if gridArr[x][y] == "o":
-
-                    # Put all of the os in a list 
-                    # Then count the amount of os
-                     
-                    # The problem arises when we have 
-                    # to find dead cells with three neighbours
                     
                 try:
-                    n = gridArr[x][y + 1]
+                    nw = gridArr[x - 1][y - 1]
+                    neighbours.append(nw)
+                except IndexError: 
+                    nw = " "
+                    neighbours.append(nw)
+
+                try:
+                    n = gridArr[x][y - 1]
                     neighbours.append(n)
                 except IndexError: 
-                    n = ""
+                    n = " "
                     neighbours.append(n)
 
                 try:
-                    ne = gridArr[x - 1][y + 1]
+                    ne = gridArr[x + 1][y - 1]
                     neighbours.append(ne)
                 except IndexError: 
-                    ne = ""
+                    ne = " "
                     neighbours.append(ne)
-
-                try:
-                    nw = gridArr[x + 1][y + 1]
-                    neighbours.append(nw)
-                except IndexError: 
-                    nw = ""
-                    neighbours.append(nw)
 
                 try:
                     w = gridArr[x - 1][y]
                     neighbours.append(w)
                 except IndexError: 
-                    w = ""
+                    w = " "
                     neighbours.append(w)
 
                 try:
                     e = gridArr[x + 1][y]
                     neighbours.append(e)
                 except IndexError: 
-                    e = ""
+                    e = " "
                     neighbours.append(e)
 
                 try:
-                    se = gridArr[x - 1][y - 1]
-                    neighbours.append(se)
-                except IndexError: 
-                    se = ""
-                    neighbours.append(se)
-
-                try:
-                    s = gridArr[x][y - 1]
-                    neighbours.append(s)
-                except IndexError: 
-                    s = ""
-                    neighbours.append(s)
-
-                try:
-                    sw = gridArr[x + 1][y - 1]
+                    sw = gridArr[x - 1][y + 1]
                     neighbours.append(sw)
                 except IndexError: 
-                    sw = ""
+                    sw = " "
                     neighbours.append(sw)
+
+                try:
+                    s = gridArr[x][y + 1]
+                    neighbours.append(s)
+                except IndexError: 
+                    s = " "
+                    neighbours.append(s)
+
+                try:
+                    se = gridArr[x + 1][y + 1]
+                    neighbours.append(se)
+                except IndexError: 
+                    se = " "
+                    neighbours.append(se)
 
                 c = gridArr[x][y]
                 neighbours.append(c)
 
-                count = 0
-                o = 0
-                #for i in neighbours:
-                    #if c < 9 :
-                        
+    currentCells = []
+    for i in range(len(neighbours)):
 
-    print(neighbours)
+        # Differentiate between an alive and a dead cell
+        # Then it should be a case of determing how many os there are
+        # And changing cells accordingly
 
-                    
+        # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        # Any live cell with two or three live neighbours lives on to the next generation.
+        # Any live cell with more than three live neighbours dies, as if by overpopulation.
+        # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+        try:
+            currentCells.append(neighbours[((i * 9) - 1)]) 
+        except IndexError:
+            break         
+
+    del currentCells[0]
+    
                     
 
 interGrid()
